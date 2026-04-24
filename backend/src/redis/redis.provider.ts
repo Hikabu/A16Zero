e3bce4892ae22767ceadff27ccb4d4b85ea37aac
Valeria <70722334+Hikabu@users.noreply.github.com>
2026-04-24 11:12:00 +0700

    fix: dockerfile prisma, prima can not reach db, redis ENOTFOUND

--- backend/src/redis/redis.provider.ts
+++ backend/src/redis/redis.provider.ts
@@ -1,8 +1,10 @@
 import Redis from 'ioredis';
 
 export const RedisProvider = {
+  
   provide: 'REDIS',
   useFactory: () => {
+    console.log('Connecting to Redis at:', process.env.REDIS_HOST);
     return new Redis({
       host: process.env.REDIS_HOST || 'localhost',
       port: 6379,
